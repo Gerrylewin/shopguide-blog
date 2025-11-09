a598a2ff5e077eb69269799119662183ebb6fe61
Isaac Lewin <128657368+Gerrylewin@users.noreply.github.com>
2025-11-09 12:03:33 -0500

    Update font imports and styles in layout and tailwind CSS. Replace Space_Grotesk with Archivo and add Questrial for body text. Adjust CSS variables for font families and enhance typography settings for headings and body text.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 import 'remark-github-blockquote-alert/alert.css'
 
-import { Space_Grotesk } from 'next/font/google'
+import { Archivo, Questrial } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider, SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
@@ -12,10 +12,17 @@ import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
 
-const space_grotesk = Space_Grotesk({
+const archivo = Archivo({
   subsets: ['latin'],
   display: 'swap',
-  variable: '--font-space-grotesk',
+  variable: '--font-archivo',
+})
+
+const questrial = Questrial({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+  variable: '--font-questrial',
 })
 
 export const metadata: Metadata = {
@@ -64,7 +71,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang={siteMetadata.language}
-      className={`${space_grotesk.variable} scroll-smooth`}
+      className={`${archivo.variable} ${questrial.variable} scroll-smooth`}
       suppressHydrationWarning
     >
       <link
