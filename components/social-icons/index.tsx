47f554d1ea969d320e473d493a1ab359d2588fee
Isaac Lewin <128657368+Gerrylewin@users.noreply.github.com>
2025-11-14 19:46:17 -0500

    Update SocialIcon component styles for improved accessibility and visual consistency; enhance author bio with a link to Country Life Natural Foods website.

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -45,14 +45,14 @@ const SocialIcon = ({ kind, href, size = 8 }: SocialIconProps) => {
 
   return (
     <a
-      className="text-sm text-gray-500 transition hover:text-gray-600"
+      className="text-sm text-primary-500 dark:text-primary-400 transition hover:text-primary-600 dark:hover:text-primary-300"
       target="_blank"
       rel="noopener noreferrer"
       href={href}
     >
       <span className="sr-only">{kind}</span>
       <SocialSvg
-        className={`hover:text-primary-500 dark:hover:text-primary-400 fill-current text-gray-700 dark:text-gray-200 h-${size} w-${size}`}
+        className={`fill-current text-primary-500 dark:text-primary-400 hover:text-primary-600 dark:hover:text-primary-300 h-${size} w-${size}`}
       />
     </a>
   )
