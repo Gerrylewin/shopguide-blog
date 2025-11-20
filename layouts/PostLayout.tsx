c67aff3e98831bc930d3be03a67daccaa493d46e
Isaac Lewin <128657368+Gerrylewin@users.noreply.github.com>
2025-11-20 07:40:41 -0500

    Center AppLogo images on mobile for top 10 blog post only

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -56,7 +56,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
       <BlogAd />
       <SectionContainer>
         <ScrollTopAndComment />
-        <article>
+        <article data-post-slug={slug}>
           <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
             <header className="pt-6 xl:pb-6">
               <div className="space-y-1 text-center">
